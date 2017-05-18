5a6a8f3139a2489a91f7b3d7547e759c0f4a942c
susmithagudapati <susmitha.gudapati@gmail.com>
2017-05-18 23:47:11 +0530

    add deleteContact spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -6,4 +6,12 @@ describe('Address Book', function() {
 
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
+
+    it('should be able to delete a contact', function() {
+        var addressBook = new AddressBook();
+        var thisContact = new Contact();
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
 });
